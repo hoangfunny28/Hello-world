502be01770d223c67fca055c0614138fcbdcdc85
hoangfunny28 <kizhoang28@gmail.com>
2020-10-21 23:51:47 +0700

    u15,u17,lab8p1

---
+++ Lab8-p1/Lab8-p1/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Contacts;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp.UI.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lab8_p1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Products = new ObservableCollection<Product>();
+            this.DataContext = this;
+        }
+        public ObservableCollection<Product> Products { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = txtproductName.Text;
+            if (productName != null)
+            {
+                Product product = new Product { Name = productName };
+                this.Products.Add(product);
+            }
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+    }
+}
+
+ 
